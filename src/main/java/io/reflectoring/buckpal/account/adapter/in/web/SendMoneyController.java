2032b8f73f5257d6a3f10f33ffbf0a41cd6dd734
jinheonyoon <yinheon@gmail.com>
2026-02-09 10:28:34 +0900

    feat: add send money controller

--- src/main/java/io/reflectoring/buckpal/account/adapter/in/web/SendMoneyController.java
+++ src/main/java/io/reflectoring/buckpal/account/adapter/in/web/SendMoneyController.java
@@ -1,4 +1,39 @@
 package io.reflectoring.buckpal.account.adapter.in.web;
 
+
+import io.reflectoring.buckpal.account.application.port.in.SendMoneyCommand;
+import io.reflectoring.buckpal.account.application.port.in.SendMoneyService;
+import io.reflectoring.buckpal.account.domain.Account.AccountId;
+import io.reflectoring.buckpal.account.domain.Money;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class SendMoneyController {
+
+    private final SendMoneyService sendMoneyService;
+
+
+    @PostMapping("/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
+    public void sendMoney(
+            @PathVariable("sourceAccountId") Long sourceAccountId,
+            @PathVariable("targetAccountId") Long targetAccountId,
+            @PathVariable("amount") Long amount
+    ) {
+
+        // 1. convert http request to command
+
+        SendMoneyCommand command = new SendMoneyCommand(
+                new AccountId(sourceAccountId),
+                new AccountId(targetAccountId),
+                Money.of(amount));
+        )
+
+        // 2. call usecase
+
+        sendMoneyService.sendMoney(command);
+    }
 }
