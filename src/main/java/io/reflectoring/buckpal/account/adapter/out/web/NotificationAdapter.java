4f332b96d96d4f4a1c3c89c97b286e54aab447e6
jinheonyoon <yinheon@gmail.com>
2026-02-09 10:29:19 +0900

    feat: add notification adapter

--- src/main/java/io/reflectoring/buckpal/account/adapter/out/web/NotificationAdapter.java
+++ src/main/java/io/reflectoring/buckpal/account/adapter/out/web/NotificationAdapter.java
@@ -1,4 +1,24 @@
 package io.reflectoring.buckpal.account.adapter.out.web;
 
-public class NotificationAdapter {
+
+import io.reflectoring.buckpal.account.application.port.out.Notification;
+import io.reflectoring.buckpal.account.application.port.out.SendNotificationPort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NotificationAdapter implements SendNotificationPort {
+
+
+    private final WebClient webClient;
+
+
+    @Override
+    public void sendNotification(Notification notification) {
+
+    }
 }
